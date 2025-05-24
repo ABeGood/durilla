cbd17000259304f81de834d82262f331184ed121
ABeGood <openzzggl@gmail.com>
2025-05-24 12:09:36 +0200

    Suffle letters

--- d_mobile_app/lib/home_screen.dart
+++ d_mobile_app/lib/home_screen.dart
@@ -1,7 +1,90 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // List of all English capital letters
+  final List<String> _englishLetters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+
+  // Current letters to display (initially DURILLA)
+  List<String> _currentLetters = ['D', 'U', 'R', 'I', 'L', 'L', 'A'];
+
+  // Available colors for letters
+  final List<Color> _availableColors = [
+    Color(0xFFFF9AD6), // Pink
+    Color(0xFF3A85FF), // Blue
+    Color(0xFF34C759), // Green
+    Color(0xFFFFCC00), // Yellow
+    Color(0xFF5FDFFF), // Light Blue
+    Color(0xFFBF3EFF), // Purple
+    Color(0xFFFF6B6B), // Red
+    Color(0xFF4ECDC4), // Teal
+    Color(0xFFFFE66D), // Light Yellow
+    Color(0xFF95E1D3), // Mint Green
+  ];
+
+  // Current colors for each letter position
+  List<Color> _currentColors = [
+    Color(0xFFFF9AD6), // D - Pink
+    Color(0xFF3A85FF), // U - Blue
+    Color(0xFF34C759), // R - Green
+    Color(0xFFFFCC00), // I - Yellow
+    Color(0xFFFF9AD6), // L - Pink
+    Color(0xFF5FDFFF), // L - Light Blue
+    Color(0xFFBF3EFF), // A - Purple
+  ];
+
+  final Random _random = Random();
+
+  // Function to generate random letters and shuffle colors
+  void _generateRandomLetters() {
+    setState(() {
+      // Generate random letters
+      _currentLetters = List.generate(
+        7, // Keep the same number of letters as DURILLA
+        (index) => _englishLetters[_random.nextInt(_englishLetters.length)],
+      );
+
+      // Shuffle colors randomly
+      _currentColors = List.generate(
+        7,
+        (index) => _availableColors[_random.nextInt(_availableColors.length)],
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,15 +121,13 @@ class HomeScreen extends StatelessWidget {
                   FittedBox(
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        _ColorLetter('D', Color(0xFFFF9AD6)),
-                        _ColorLetter('U', Color(0xFF3A85FF)),
-                        _ColorLetter('R', Color(0xFF34C759)),
-                        _ColorLetter('I', Color(0xFFFFCC00)),
-                        _ColorLetter('L', Color(0xFFFF9AD6)),
-                        _ColorLetter('L', Color(0xFF5FDFFF)),
-                        _ColorLetter('A', Color(0xFFBF3EFF)),
-                      ],
+                      children: List.generate(
+                        _currentLetters.length,
+                        (index) => _ColorLetter(
+                          _currentLetters[index],
+                          _currentColors[index],
+                        ),
+                      ),
                     ),
                   ),
                   const SizedBox(height: 64),
@@ -58,6 +139,7 @@ class HomeScreen extends StatelessWidget {
                       child: ElevatedButton(
                         onPressed: () {
                           debugPrint('Start Game нажато');
+                          _generateRandomLetters();
                         },
                         style: ElevatedButton.styleFrom(
                           backgroundColor: const Color(0xFF34C759),
